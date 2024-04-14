89fe1d96a2435ce74e6842639f966a78270ea06a
D.mon 24 <dchaminov@ya.ru>
2024-04-14 14:00:02 +0300

    Refactoring: comments, renaming, clearing

--- ResourceMapper.m
+++ ResourceMapper.m
@@ -1,22 +1,25 @@
 classdef ResourceMapper<handle
 
     properties
-        resourceGrid
-        mu
-        frameCount
-        isCycledPrefixExtended
+        resourceGrid    % main grid
+        mu              % Subcarrier spacing configuration
+        frameCount      % amount of frames in resourceGrid
+        isCycledPrefixExtended  % cycled prefix flag
     end
-    methods(Static)
-        function pbchDmRs=preparePbchDmRs(pbchDmRs)
-            pbchDmRs=pbchDmRs+0;
-        end
-        function pbch=preparePbch(pbch)
-            pbch=pbch+0;
-        end
-    end
+
     methods
+        
+        function pbchDmRs=preparePbchDmRs(obj,pbchDmRs)
+            % pre-mapping staff
+            pbchDmRs=pbchDmRs+0; %todo
+        end
+        function pbch=preparePbch(obj,pbch)
+             % pre-mapping staff
+            pbch=pbch+0; %todo
+        end
+    
         function obj = ResourceMapper()
-            %pass;
+            % empty constructor;
         end
 
         
@@ -27,24 +30,23 @@ classdef ResourceMapper<handle
                 frameCount              (1,1)
                 isCycledPrefixExtended  (1,1) =false
             end
+
             if(isCycledPrefixExtended) % ONLY FOR MU==2
                 %   12 sym per slot, 10*4 slot per frame = 480 symb per frame
-                obj.resourceGrid=zeros(4,480*frameCount);
+                obj.resourceGrid=zeros(4,480*frameCount); % FIXME 
             else
-                obj.resourceGrid=zeros(240,2^mu*140*frameCount);
+                obj.resourceGrid=zeros(240,2^mu*140*frameCount);% FIXME
             end
+            % initializing
             obj.mu=mu;
             obj.isCycledPrefixExtended=isCycledPrefixExtended;
             obj.frameCount=frameCount;
         end
         
         %%% ===============================================================
-        %%% ====================     cool code    =========================
-        %%% ===============================================================
-        
+      
         
         % SS MAPPING
-        
         function obj = addPssToResourceGrid(obj,PssSignal,offset)
         %adds PSS to resource matrix
             arguments
@@ -53,8 +55,9 @@ classdef ResourceMapper<handle
                 offset (1,1) = 2 %optional
             end
             for i = 0:obj.frameCount*10*2^obj.mu-1 %frame amount * 10 subframes * 2^mu slots
-                    obj.resourceGrid(57:183,1+offset+14*i) = fft(PssSignal.').';% one slot has
-                    obj.resourceGrid(57:183,1+offset+6+14*i) = fft(PssSignal.').';% 2 PSS
+                % one slot has 2 PSS
+                obj.resourceGrid(57:183,1+offset+14*i) = fft(PssSignal.').';
+                obj.resourceGrid(57:183,1+offset+6+14*i) = fft(PssSignal.').';
             end
         end
         
@@ -65,19 +68,24 @@ classdef ResourceMapper<handle
                 SssSignal (1,127)
                 offset (1,1) = 4 %optional
             end
-            for i = 0:obj.frameCount*10*2^obj.mu-1 %frame amount * 10 subframes * 2^mu slots
-                obj.resourceGrid(57:183,1+offset+14*i) = fft(SssSignal.').';% one slot has
-                obj.resourceGrid(57:183,1+offset+6+14*i) = fft(SssSignal.').';% 2 SSS
+            %frame amount * 10 subframes * 2^mu slots
+            for i = 0:obj.frameCount*10*2^obj.mu-1 
+                % one slot has 2 SSS
+                obj.resourceGrid(57:183,1+offset+14*i) = fft(SssSignal.').';
+                obj.resourceGrid(57:183,1+offset+6+14*i) = fft(SssSignal.').';
             end
         end
         
         % PBCH MAPPING
         function obj = addPbchToResourceGrid(obj,NCellId,pbch)
+           % for each slot in frames
            for i=0:1:obj.frameCount*2^obj.mu*10-1
+               % 2 pbch parts per slot
                 obj=obj.addPbchPartToResourceGrid(NCellId,pbch(i+1,:),2+i*14);
                 obj=obj.addPbchPartToResourceGrid(NCellId,pbch(i+1,:),8+i*14);
            end 
         end
+
         function obj = addPbchPartToResourceGrid(obj,NCellId,pbch,shift)
             arguments
                 obj
@@ -85,8 +93,12 @@ classdef ResourceMapper<handle
                 pbch
                 shift =0
             end
+
+            % nu parameter for shift of DM-RS
             nu=mod(NCellId,4);
+            % pre-mapping staff
             pbch=ResourceMapper.preparePbch(pbch);
+            
             %throwing out dmrs indexes
             indexes=find(mod(1:1:240,4)~=(nu+1));
 
@@ -94,13 +106,15 @@ classdef ResourceMapper<handle
             obj.resourceGrid(indexes,2+shift)=pbch(1:180);
             % mapping last 180
             obj.resourceGrid(indexes,4+shift)=pbch(end-179:end);
-            %mapping arround SSS
+            % mapping arround SSS
             indexes=indexes(indexes<49 | indexes>192);
             obj.resourceGrid(indexes,3+shift)=pbch(181:181+71);
         end
 
         function obj=addPbchDmRsToResourceGrid(obj,NCellId,pbchDmRs)
+            % for each slot in grid
             for i=0:1:obj.frameCount*2^obj.mu*10-1
+                 % twice in each slot of the subframe
                 obj=obj.addPbchDmRsPartToResourceGrid(NCellId,pbchDmRs(i+1,:),2+i*14);
                 obj=obj.addPbchDmRsPartToResourceGrid(NCellId,pbchDmRs(i+1,:),8+i*14);
             end
@@ -114,26 +128,31 @@ classdef ResourceMapper<handle
                 pbchDmRs
                 shift =0
             end
+            % nu parameter for shift of DM-RS
             nu=mod(NCellId,4);
+            % pre-mapping staff
             pbchDmRs=ResourceMapper.preparePbchDmRs(pbchDmRs);
             % first 60 PBCH DM-RS
             dmrs=pbchDmRs(1:60);
+            
+            % indexes array
             indexes=find(mod(1:1:240,4)==1);
+            % mapping 1st part
             obj.resourceGrid(indexes+nu,2+shift)=dmrs;
             % d---d---d---d … d---d---
             
             % last dm-rs block
             dmrs=pbchDmRs(end-59:end);
+            % mapping 2nd part
             obj.resourceGrid(indexes+nu,4+shift)=dmrs;
             % d---d---d---d … d---d---
 
             % next dm-rs block (24 elements are splitted into two blocks)
             dmrs=pbchDmRs(62:62+23);
             indexes=indexes(indexes<46 | indexes>192); % throwing SSS area
+            % mapping 3rd part
             obj.resourceGrid(indexes+nu,3+shift)=dmrs;
             % d---d---d--…-SSS-…-d---d--d
-
-            
         end
     end
     
