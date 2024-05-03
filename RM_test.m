26161eefe5b0d1422988d73fdfd2268508d5cc6a
D.mon 24 <dchaminov@ya.ru>
2024-05-03 15:02:18 +0300

    tests update

--- RM_test.m
+++ RM_test.m
@@ -11,5 +11,5 @@ r.createResourceGrid(mu,frameCount);
 
 r.addSsBlockByCase('G',0:8,ID,pss,sss,ones(5*500,432)*20,ones(5*500,144)*40,0,0);
 
-h=heatmap(abs(r.resourceGrid(:,100:end)));
+h=heatmap(abs(r.resourceGrid(1:300,100:end)));
 grid off
