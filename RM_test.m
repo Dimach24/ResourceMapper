dc6eeaedd9a40e96b441d08a4b896d0912cecf09
D.mon 24 <dchaminov@ya.ru>
2024-05-15 21:31:11 +0300

    test update

--- RM_test.m
+++ RM_test.m
@@ -5,12 +5,17 @@ addpath(genpath(pwd));
 %%
 frameCount = 6;
 ID=4; %ncell_ID
-mu = 4;
+mu = 1;
 [pss,sss]=SsGenerator.getSsSignalsByCellInfo(ID);
 r=ResourceMapper();
-r.createResourceGrid(mu,frameCount);
+r.createResourceGrid(mu,frameCount,false,30);
 
-r.addSsBlockByCase('G',0:8,ID,pss,sss,ones(5*500,432)*20,ones(5*500,144)*40,0,0);
+r.addSsBlockByCase('B',0:8,ID,pss,sss,ones(5*500,432)*20,ones(5*500,144)*40,0,0);
 
-h=heatmap(abs(r.resourceGrid(1:300,100:end)));
+s=pcolor(abs(r.resourceGrid(1:260,1:80)));
+s.EdgeColor='none';
 grid off
+a=gca();
+a.YDir='normal';
+xlabel('l (номер OFDM символа)')
+ylabel('k (номер поднесущей)')
